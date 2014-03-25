6118d52f900066d8c6a781a8d14aff71b33e8a53
Salariel <jr.salatiel@gmail.com>
2014-03-24 22:57:35 -0300

    first commit new branch with maven project configured.

--- notas-fiscais/src/br/com/notasfiscais/dao/GraficoDao.java
+++ notas-fiscais/src/br/com/notasfiscais/dao/GraficoDao.java
@@ -10,14 +10,16 @@ import br.com.notasfiscais.modelo.QuantidadePorProduto;
 
 public class GraficoDao implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	
-	@Inject
-	private EntityManager manager;
+    private static final long serialVersionUID = 1L;
 
-	public List<QuantidadePorProduto> relatorioQuantidadePorProduto() {
-		return manager.createQuery(
-			  "select new br.com.caelum.notasfiscais.modelo.QuantidadePorProduto(sum(i.quantidade), i.produto) "
-			+ " from Item i group by i.produto" , QuantidadePorProduto.class).getResultList();
-	}
+    @Inject
+    private EntityManager manager;
+
+    public List<QuantidadePorProduto> relatorioQuantidadePorProduto() {
+	return manager
+		.createQuery(
+			"select new br.com.notasfiscais.modelo.QuantidadePorProduto(sum(i.quantidade), i.produto) "
+				+ " from Item i group by i.produto",
+			QuantidadePorProduto.class).getResultList();
+    }
 }
