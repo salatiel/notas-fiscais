c7d218fc0fbcfe553ac593963dfc0a42ddc1b360
Salariel <jr.salatiel@gmail.com>
2014-03-29 23:31:23 -0300

    Check for user logged in

---
+++ notas-fiscais/src/br/com/notasfiscais/listener/ValidatorUserLoggedListener.java
@@ -0,0 +1,56 @@
+package br.com.notasfiscais.listener;
+
+import javax.faces.application.NavigationHandler;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+import javax.inject.Inject;
+
+import br.com.notasfiscais.util.UsuarioLogado;
+
+public class ValidatorUserLoggedListener implements PhaseListener {
+
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    private UsuarioLogado usuarioLogado;
+
+    private String[] pagesAuthoride = { "login", "index" };
+
+    @Override
+    public void afterPhase(PhaseEvent event) {
+	if (!usuarioLogado.isLogado()) {
+
+	    boolean authorized = false;
+	    
+	    FacesContext context = event.getFacesContext();
+
+	    String view = context.getViewRoot().getViewId();
+
+	    for (String p : pagesAuthoride) {
+		if (view.contains(p)) {
+		    authorized = true;
+		}
+	    }
+	    if(!authorized){
+		NavigationHandler handler = context.getApplication().getNavigationHandler();
+		
+		handler.handleNavigation(context, null, "login?faces-redirect=true");
+		
+		context.renderResponse();
+	    }
+	}
+    }
+
+    @Override
+    public void beforePhase(PhaseEvent event) {
+
+    }
+
+    @Override
+    public PhaseId getPhaseId() {
+	return PhaseId.RESTORE_VIEW;
+    }
+
+}
